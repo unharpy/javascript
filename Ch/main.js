b12cbad5dfde37c90bb4aba81c85c8e2d1382077
unharpy <115774947+unharpy@users.noreply.github.com>
2023-09-15 20:28:43 +0900

    Add files via upload

--- Ch/main.js
+++ Ch/main.js
@@ -1,127 +1,140 @@
 /*
-데이터 타입
- - 값의 종류를 의마하고, 기본 타입과 객체 타입으로 나뉜다.
+연산자
+ - 어떠한 연산을 기호를 사용하여 수행한다.
+*/
+
+/*
+문자열 병합
+ - 문자열은 + 연산자를 사용하여 병합할 수 있다.
+ */
+console.log("안녕"+"하세용"+"내일만 지나면"+"주말이에용");
+console.log(typeof ("1"+2)); // 문자열과 병합시 number도 string이 된다.
+console.log("1"+2+3);
+console.log(1+2+"3"); 
+
+/*
+산술 연산자 
+  x + y : 덧셈
+  x - y : 뺄셈
+  x * y : 곱셈 
+  x / y : 나눗셈
+  x % y : 나머지   
+*/
+let x=10;
+let y=2;
+console.log(x*y);
+console.log(x%y);
+console.log(x+y);
+
+/*
+증강 연산자
+ ++x : 전위 증가 (x에 1을 증가시킨 후에 명령문 수행)
+ x++ : 후위 증가 (현재 명령문을 수행 후 x에 1을 증가)
+ --x : 전위 감소 (x에 1을 감소시킨 후에 명령문 수행)
+ x-- : 후위감소 (현재 명령문을 수행 후에 x에 1을 감소)
+*/
+
+// let result = ++x +y; 전위 증가
+// let result = x++ +y; 후위 증가
+// let result = --x +y; 전위 감소
+// let result = x-- +y; 후위 감소
+// console.log(result);
+
+// let result1 = x-- *y;
+// console.log(result1);
+let result = x++ +y;
+
+console.log(result);
+console.log(x);
+console.log(y);
+
+/*
+비교 연산자
+ a > b   : a가 b보다 크다.
+ a < b   : a가 b보다 작다.
+ a >= b  : a가 b보다 크거나 같다
+ a =< b  : a가 b보다 작거나 같다
+ a == b  : a와 b는 같다
+ a === b : a와 b는 같다(데이터 타입 까지 같다)
+ a != b  : a와 b는 같지 않다.
+ a !== b : a와 b는 같이 않다.(데이터 타입 까지)
+*/
+let a = 2;
+let b = 2;
+console.log(a>b);
+console.log(a >= b);
+console.log(a==b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a===b);
+
+/*
+할당 연산자
+ x += y : x는 x + y와 같다
+ x -= y : x는 x - y와 같다
+ x *= y : x는 x * y와 같다
+ x /= y : x는 x / y와 같다
+*/
+
+let i = 10;
+let j = 20;
+i += j;
+console.log(i) 
+// i += j;
+// console.log(i)       x는 x + y와 같다  
+// i -= j;
+// console.log(i)       x는 x - y와 같다
+// i *= j;
+// console.log(i)       x는 x * y와 같다
+// i /= j;
+// console.log(i)       x는 x / y와 같다
+
+/*
+논리 연산자 
+ - 논리 연산자는 boolean값만 다룰 수 있지만 자바스크립트에서만 
+   모든 타입 값을 받을 수 있고 결과 역시 모든 타입이 될 수 있다.
+ - null, 0 , NaN, 빈 문자열(""), undefined는 false로 간주한다.
+AND 연산자 (&&)
+ - 피연산자가 모두 true일 경우에 true를 반환하고 나머지는 false로 반환
+
+OR 연산자 (||) 
+ - 피 연산자가 하나라도 true면 true를 반환하고 나머지는 false를 반환
+
+NOT 연산자 (!)
+ - true면 false를 반환하고 false면 true를 반환한다.
+
+ 단축평가
+  - AND 연산자에서 앞쪽 피연산자가 false일 때,
+    OR 연산에서 앞쪽 피연산자가 true일때 나머지 피연산자의 평가를 생략하고 그대로 반환.
  */
 
- /*
- 기본타입
-  - 값을 하나만 가지며 원시값이라고도 불린다.
-  - number(숫자), string(문자열), boolean, null, undefined, symbol
+let a1 = true && true;
+let a2 = true && false && true;
+let a3 = true && (3 == 5);
+let a4 = 111 && 222;        // 문자나 string이 나오면 기본적으로는 true and 연산자는 둘다 트루가 되어야만 되므로 둘다 확인해서 후에 나오는값이 출력이됨 
+let a5 = 111 && false;
+let a6 = null && undefined;
+console.log(a6);
 
- 객체 타입
-  - 기본 타입을 제외한 값은 모두 객체 타입이다.
-  - 오브젝트(Object) 배열(Array) 함수(Function) 등 다양한 객체 존재
+let o1 = true || true;
+let o2 = true || false;
+let o3 = false || false;
+let o4 = false || true;
+let o5 = 111 || 222;
+let o6 = false || 222;
+let o7 = null || undefined;
+console.log(o6);
 
- typeof
-  - 피연산자의 데이터 타입을 문자열로 반환한다 - 데이터 타입 확인용 키워드.
-*/
-console.log(typeof "123123");
-
-/*
-숫자
- - number 타입이라고 합니다.
- - 자바나 다른 언어는 정수, 데이터 크기에 따라 키워드가 다양하지만 
-   자바스크립트는 그냥 숫자만 적으면된다.
-   
- - 숫자 타입에서 특별한 값
-  - Infinity - 양수를 0으로 나눈 경우
-  - -Infinity - 음수를 0으로 나눈 경우
-  - NaN : 문자열을 나눈경우
-*/
-let number1 = 1;
-const number2 = 2;
-console.log(number2);
-
-let number3 = 1;
-const number4 = "안녕" /0 ;
-console.log(number4);
+let n1 = !true;
+let n2 = !false;
+let n3 = !1111;
+console.log(n2);
 
 /*
-문자열 
- - string 타입이라고 한다.
- - 문자열을 표현할 땐 작은따음표(''), 큰따음표(""), 백틱(``) 사용
- - 백틱(``) ES6버전에서 유입되었다.
-
-
- 백틱
-  - 여러줄 표현이 가능하다.
-  - 탬플릿 문자열을 더 편리하게 사용이 가능하다.
-
-탬플릿 문자열
-  - 값과 문자열을 + 기호를 사용하여 연결한다.
-  - 백틱을 사용하면 더 편리하게 연결이 가능하다.
+삼항 연산자
+ - 조건식 ? 값1: 값2
+ - 조건식이 true이면 값1을 반환하고, false이면 값2를 반환한다.
 */
-
-let nameA = '홍길동';
-let nameB = "박길동";
-let nameC = `유길동`;
-console.log(typeof nameA);
-console.log(typeof nameB);
-console.log(typeof nameC);
-
-// 백틱은 여려줄 표현이 가능
-let temp1 = `안녕하세요
-반갑습니다.
-퇴근을 했는데
-집이 아니에요`;
-console.log(temp1);
-
-//탬플릿 문자열
-let nameD = "유중근";
-let age = 88;
-let city = "대전";
-console.log("내 이름은"+ nameD + "입니다. 나이는" + age + "입니다");
-
-//백틱 사용
-
-console.log(`내 이름은 ${nameD} 입니다. 사는곳은 ${city} 나이는 ${age}입니다`);
-
-/*
-이스케이프 표현
- - 일반적인 문자 외에 특수 문자는 이스케이프 표현을 사용한다.
- - \n 줄바꿈
- - \\ 백스페이스
- - \"" 큰따음표
- - '' 작은따음표
- - \t 탭
-*/
-console.log("안녕하세요\n반갑습니다.");
-console.log("안녕하세요\"반갑습니다.\"");
-console.log("안녕하세요\t반갑습니다.");
-
-/*
-boolean(불린)
- - true 와 false 두가지 값만 가지는 논리형 데이터 타입이다.
-*/
-let isTrue = true;
-let isFalse = false;
-console.log(typeof isTrue);
-console.log(typeof isFalse);
-
-
-/*
-심볼(symbol)
- - 유일한 값을 나타내기 위해 ES6버전에서 도입한 데이터 타입이다.
- - 고유한 식별자가 필요할 때 사용한다.
- */
-
- // symbol을 줌으로써 값은 같지만 고유한 식별자가 되었으므로 symbol값이 달라져 flase가 됨
-let key1 = Symbol("abc");
-let key2 = Symbol("abc");
-console.log(key1 ==key2); // symbol값은 서로 다름.
-console.log(key1.description); // symbol값 출력
-
-let key3 = "abc";
-let key4 = "abc";
-console.log(key3 ==key4);
-
-/*
-null, undefined
- - 둘 다 값이 비어있는 상태이다.
- - null : 값이 없는 상태
- - undefined : 값을 할당하지 않은 상태
-*/
-let message1 = null;    // 값이 null인 빈 상태를 표현
-let message2 = undefined;   // 값이 할당이 안된상태
-console.log(message1);
-console.log(message2);
+let number = 11;
+let result2 = number % 2 == 1 ? "홀수입니다." : "짝수입니다.";
+console.log(result2);
